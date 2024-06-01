af09d4696ed0ce5f35f0c5f6cf39e8c865706e21
Sto0gna <virusplay97@gmail.com>
2024-06-01 17:57:04 +0300

    small image optimizing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="header">
-        <img class="header__img logo" src="../assets/images/Logo.svg">
+        <img class="header__img logo" fetchpriority="high" src="../assets/images/Logo.svg" width="104px" height="26px">
         <div class="header_button-group button-group">
             <Button class="header_button-group__button button" msg="Users" type="yellow" @click="openUsers"></Button>
             <Button class="header_button-group__button button" msg="Sign up" type="yellow" @click="signUpAnchor"></Button>
